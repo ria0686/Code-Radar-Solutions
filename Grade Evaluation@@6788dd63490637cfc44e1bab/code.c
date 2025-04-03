8d6ba738d862859bf32f933a902694f1155bdc91
ria0686 <ria0686.becse24@chitkara.edu.in>
2025-04-03 20:00:19 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,17 @@
+// Your code here...
+#include <stdio.h>
+
+int main()
+{
+    char x;
+    scanf("%c",&x);
+    switch(x)
+    {
+        case 'A': printf("Excellent");break;
+        case 'B': printf("Good");break;
+        case 'C': printf("Average");break;
+        case 'D': printf("Below Average");break;
+        case 'F': printf("Fail");break;
+        default:printf("Invalid");
+    }
+}
