9f2f441d388d8a3474d8619a8a940a674965a47f
ria0686 <ria0686.becse24@chitkara.edu.in>
2025-04-03 15:05:54 +0530

    Solution to Determine Eligibility for Voting

---
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -0,0 +1,15 @@
+// Your code here...
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    if(a>=18)
+    {
+        printf("Eligible");
+    }
+    else
+    {
+        printf("Not Eligible");
+    }
+}
